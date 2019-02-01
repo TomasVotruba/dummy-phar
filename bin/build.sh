26a6ba3e86da2d5c205c4ba31307c3912c2ad564
Tomas Votruba <tomas.vot@gmail.com>
2019-02-01 15:55:08 +0100

    simplify build

--- bin/build.sh
+++ bin/build.sh
@@ -13,10 +13,6 @@ cp composer.json build/composer.json
 cp box.json build/box.json
 cp scoper.inc.php build/scoper.inc.php
 
-# composer
-composer require --no-update dg/composer-cleaner:^2.0 -d build
-composer update --no-dev --classmap-authoritative -d build
-
 # build + prefix
 mkdir -p tmp/build
 vendor/bin/box compile --config build/box.json -vvv
