110618462e1a7c07bbf0f526c30860f83133e765
Tomas Votruba <tomas.vot@gmail.com>
2019-02-02 23:59:50 +0100

    fix

--- bin/build.sh
+++ bin/build.sh
@@ -7,12 +7,12 @@ set -o xtrace
 mkdir -p build/bin
 cp bin/run build/bin/run
 cp -r src build/src
+cp -r vendor build/vendor
 cp composer.json build/composer.json
 
 # copy configs
 cp box.json build/box.json
-cp scoper.inc.php build/scoper.inc.php
 
 # build + prefix
 mkdir -p tmp/build
-vendor/bin/box compile --config build/box.json -vvv
+./box.phar compile --working-dir build
